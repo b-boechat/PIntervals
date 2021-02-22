0d01d67f77e09aaff79dc435b1feeb69791df192
b-boechat <bernardoant@poli.ufrj.br>
2021-02-22 12:09:55 -0300

    Install Charts, create placeholder View Controller and Storyboard for statistics.

---
+++ Cantar Intervalo/Cantar intervalo/BarChartView.swift
@@ -0,0 +1,11 @@
+//
+//  BarChartView.swift
+//  Cantar intervalo
+//
+//  Created by Macintosh on 21/02/21.
+//  Copyright © 2021 Bernardo. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
